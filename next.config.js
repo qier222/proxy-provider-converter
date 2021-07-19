8d6bd79c1b19e6b7675fb1ff4374f49bbd1c3b85
qier222 <qier222@outlook.com>
2021-07-20 01:46:46 +0800

    update

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  webpack(config, { dev, isServer }) {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import splitbee from "@splitbee/web";
 
 function MyApp({ Component, pageProps }) {
+  splitbee.init({
+    scriptUrl: "/bee.js",
+    apiUrl: "/_hive",
+  });
   return <Component {...pageProps} />;
 }
 
